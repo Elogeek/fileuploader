04ccf0e069395f7f97cf9857ac8f87f5493b9aec
elogeek <elodiechristin@gmail.com>
2022-07-19 14:18:35 +0200

    test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,48 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import FileUpload from "react-mui-fileuploader";
+import { TextField } from '@mui/material';
 
 function App() {
+
+  const [file, setFile] = useState();
 
   const handleFileUploadError = (error) => {
     // Do something...
   }
 
-  const handleFilesChange = (files) => {
-    // Do something...
+  const handleFilesChange = (event) => {
+      setFile(event.target.files[0])
   }
 
   return (
       <FileUpload
           multiFile={true}
           disabled={false}
-          title="My awesome file uploader"
-          header="[Drag to drop]"
-          leftLabel="or"
-          rightLabel="to select files"
-          buttonLabel="click here"
-          buttonRemoveLabel="Remove all"
+          accept="image/*"
+          maxSize={512000}
+          minSize={5120}
           maxFileSize={10}
           maxUploadFiles={0}
           maxFilesContainerHeight={357}
           errorSizeMessage={'fill it or move it to use the default error message'}
-          allowedExtensions={['jpg', 'jpeg']}
           onFilesChange={handleFilesChange}
           onError={handleFileUploadError}
-          imageSrc={'path/to/custom/image'}
           bannerProps={{ elevation: 0, variant: "outlined" }}
           containerProps={{ elevation: 0, variant: "outlined" }}
-      />
+      >
+          <div className="file-uploader">
+            <TextField
+                id="avatar-upload"
+                type="file"
+                margin="normal"
+                fullWidth
+                label="Upload a new avatar"
+                variant="filled"
+                onChange={handleFilesChange}
+            />
+          </div>
+      </FileUpload>
   )
 }
 
